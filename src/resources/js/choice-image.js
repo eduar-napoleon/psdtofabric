f4910425b645f614ad3cf2ec6da4c6b847db6ca1
enapoleon <eduar.napoleon@gmail.com>
2021-08-25 10:41:13 +0700

    init

---
+++ src/resources/js/choice-image.js
@@ -0,0 +1,89 @@
+$(function () {
+  // Eevnt: Choice meme from top 100
+  $(".memes-container").delegate("img", "click", function () {
+    var $img = $(this);
+    var imgInfo = {
+      url: $img.attr("src"),
+      height: $img.attr("img-height"),
+      width: $img.attr("img-width"),
+    };
+
+    $(".choice-section").trigger("choice-done", imgInfo);
+  });
+
+  // Event: Upload local image
+  $("#meme-input").on("change", async function () {
+    const file = this.files[0];
+    const fileType = file["type"];
+
+    // Reset file input
+    $("#meme-input").val("");
+    console.log(file);
+
+    // Validate this is image
+    if (!isImage(fileType)) {
+      if (file["name"].split(".").pop().toUpperCase() === "PSD") {
+        var imgJson = await convertPSD(file);
+        // console.log(JSON.stringify(imgJson));
+        var imgInfo = {
+          json: JSON.stringify(imgJson),
+          height: imgJson.backgroundImage.height,
+          width: imgJson.backgroundImage.width,
+        };
+        $(".choice-section").trigger("choice-done", imgInfo);
+        return;
+      }
+      const reader = new FileReader();
+      reader.onload = function () {
+        var meme = JSON.parse(reader.result);
+        if (meme.backgroundImage) {
+          var imgInfo = {
+            json: reader.result,
+            height: meme.backgroundImage.height,
+            width: meme.backgroundImage.width,
+          };
+          $(".choice-section").trigger("choice-done", imgInfo);
+        } else {
+          showAlert("Error! Invalid Image");
+        }
+      };
+      reader.readAsText(file);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function () {
+      var meme = new Image();
+      meme.src = reader.result;
+      meme.onload = function () {
+        var imgInfo = {
+          url: reader.result,
+          height: meme.height,
+          width: meme.width,
+        };
+        $(".choice-section").trigger("choice-done", imgInfo);
+      };
+    };
+    reader.readAsDataURL(file);
+  });
+
+  // Event: Choice was made
+  $(".choice-section").on("choice-done", function (e, imgInfo) {
+    $(".discription").fadeOut();
+    $(".choice-section").fadeOut("normal", function () {
+      $(".edit-section").removeClass("d-none").hide().fadeIn();
+      $(".fabric-canvas-wrapper").append(`<canvas id="meme-canvas"></canvas>`);
+      processMeme(imgInfo);
+    });
+  });
+
+  // Event: Back button click
+  $(".back-btn .btn").on("click", function () {
+    $(".edit-section").fadeOut("normal", function () {
+      $(".canvas-container").remove();
+      $(".choice-section").fadeIn();
+      $(".discription").fadeIn();
+      enableTextMethods();
+    });
+  });
+});
---
+++ src/resources/js/main.js
@@ -0,0 +1,365 @@
+var canvas;
+var PSD = require("psd");
+// Meme process
+function processMeme(memeInfo) {
+  // Responsive canvas
+  $(window).resize(resizeCanvas);
+  function resizeCanvas() {
+    var width = $(".fabric-canvas-wrapper").width();
+    $(".canvas-container").css("width", width);
+    $(".canvas-container").css(
+      "height",
+      (width * memeInfo.height) / memeInfo.width
+    );
+  }
+
+  // Intialize fabric canvas
+  canvas = new fabric.Canvas("meme-canvas", {
+    width: memeInfo.width,
+    height: memeInfo.height,
+    selection: false,
+    allowTouchScrolling: true,
+  });
+
+  // Scale is a range input allow small screen users to scale the object easily
+  $("#scale").attr("max", canvas.width * 0.0025);
+  $("#scale").val((canvas.width * 0.0025) / 2);
+
+  resizeCanvas();
+
+  // Add meme template as canvas background
+  if (memeInfo.url) {
+    fabric.Image.fromURL(
+      `${memeInfo.url}`,
+      function (meme) {
+        canvas.setBackgroundImage(meme, canvas.renderAll.bind(canvas));
+      },
+      {
+        crossOrigin: "anonymous",
+      }
+    );
+  } else if (memeInfo.json) {
+    canvas.loadFromJSON(memeInfo.json, canvas.renderAll.bind(canvas));
+  }
+
+  // Event: Add new text
+  $("#add-text")
+    .off("click")
+    .on("click", function () {
+      if ($("#text").val() == "") {
+        showAlert("Error! Text field is empty");
+        return;
+      }
+
+      // Create new text object
+      var text = new fabric.Text($("#text").val(), {
+        top: 10,
+        left: 10,
+        fontFamily: $("#font-family").find(":selected").attr("value"),
+        textAlign: $('input[name="align"]:checked').val(),
+        fontSize: $("#font-size").val(),
+        fill: $("#cp-text").colorpicker("getValue"),
+        fontStyle: $("#italic").attr("data"),
+        fontWeight: $("#bold").attr("data"),
+        underline: $("#underline").attr("data"),
+        stroke: $("#cp-stroke").colorpicker("getValue"),
+        strokeWidth: $("#stroke-width").val(),
+        shadow: createShadow(
+          $("#cp-shadow").colorpicker("getValue"),
+          $("#shadow-depth").val()
+        ),
+        textBackgroundColor: getBackgroundColor(
+          $("#cp-background").colorpicker("getValue")
+        ),
+        opacity: parseFloat($("#opacity").val() / 100),
+      });
+
+      text.scaleToWidth(canvas.width / 2);
+      $("#scale").val(text.scaleX);
+
+      canvas.add(text).setActiveObject(text);
+      loadFont(text.fontFamily);
+    });
+
+  // Event: Add new image
+  $("#add-image")
+    .off("input")
+    .on("input", function () {
+      const file = this.files[0];
+      const fileType = file["type"];
+      $("#add-image").val("");
+
+      if (!isImage(fileType)) {
+        showAlert("Error! Invalid Image");
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = function () {
+        var image = new Image();
+        image.src = reader.result;
+        image.onload = function () {
+          fabric.Image.fromURL(
+            reader.result,
+            function (image) {
+              image.scaleToWidth(canvas.width / 2);
+              canvas.add(image).setActiveObject(image);
+              $("#scale").val(image.scaleX);
+            },
+            {
+              opacity: $("#opacity").val(),
+            }
+          );
+        };
+      };
+      reader.readAsDataURL(file);
+    });
+
+  // Custom control
+  fabric.Object.prototype.set({
+    transparentCorners: false,
+    cornerColor: "yellow",
+    borderColor: "rgba(88,42,114)",
+    cornerSize: parseInt(canvas.width) * 0.03,
+    cornerStrokeColor: "#000000",
+    borderScaleFactor: 2,
+    padding: 4,
+  });
+
+  // add event listener handlers to edit methods
+  loadObjectHandlers();
+
+  // Update edit methods values to the selected canvas text
+  canvas.on({
+    "selection:created": updateInputs,
+    "selection:updated": updateInputs,
+    "selection:cleared": enableTextMethods,
+  });
+
+  $("#generate-meme")
+    .off("click")
+    .on("click", function () {
+      console.log(JSON.stringify(canvas));
+      var dataURL = canvas.toDataURL({
+        format: "png",
+      });
+
+      var link = document.createElement("a");
+      link.href = dataURL;
+      link.download = createImgName();
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+}
+
+function deltaTransformPoint(matrix, point) {
+  var dx = point.x * matrix.a + point.y * matrix.c + 0;
+  var dy = point.x * matrix.b + point.y * matrix.d + 0;
+  return { x: dx, y: dy };
+}
+
+function decomposeMatrix(matrix) {
+  // @see https://gist.github.com/2052247
+
+  // calculate delta transform point
+  var px = deltaTransformPoint(matrix, { x: 0, y: 1 });
+  var py = deltaTransformPoint(matrix, { x: 1, y: 0 });
+
+  // calculate skew
+  var skewX = (180 / Math.PI) * Math.atan2(px.y, px.x) - 90;
+  var skewY = (180 / Math.PI) * Math.atan2(py.y, py.x);
+
+  return {
+    translateX: matrix.e,
+    translateY: matrix.f,
+    scaleX: Math.sqrt(matrix.a * matrix.a + matrix.b * matrix.b),
+    scaleY: Math.sqrt(matrix.c * matrix.c + matrix.d * matrix.d),
+    skewX: skewX,
+    skewY: skewY,
+    rotation: skewX, // rotation is the same as skew x
+  };
+}
+var ddd = {
+  xx: 0.9768115389520019,
+  xy: 0.21410095135291024,
+  yx: -0.21410095135291027,
+  yy: 0.9768115389520023,
+  tx: 252.73621934210445,
+  ty: 117.96986418654765,
+};
+console.log(Math.round(Math.atan2(ddd.xy, ddd.yx) * (180 / Math.PI)));
+console.log(
+  decomposeMatrix({
+    a: ddd.xx,
+    b: ddd.yx,
+    c: ddd.tx,
+    d: ddd.xy,
+    e: ddd.yy,
+    f: ddd.ty,
+  })
+);
+
+function toFabricJson(layer, scale) {
+  var arr = layer.export();
+  if (arr.text) {
+    var dt = {
+      type: "textbox",
+      version: "4.4.0",
+      originX: "left",
+      originY: "top",
+      left: arr.left,
+      top: arr.top,
+      width: arr.width,
+      height: arr.height,
+      fill: "rgb(255, 255, 255)",
+      stroke: "#000000",
+      strokeWidth: 1,
+      strokeDashArray: null,
+      strokeLineCap: "butt",
+      strokeDashOffset: 0,
+      strokeLineJoin: "miter",
+      strokeMiterLimit: 4,
+      scaleX: 1,
+      scaleY: 1,
+      angle: 0,
+      flipX: false,
+      flipY: false,
+      opacity: 1,
+      visible: true,
+      backgroundColor: "",
+      fillRule: "nonzero",
+      paintFirst: "fill",
+      globalCompositeOperation: "source-over",
+      skewX: 0,
+      skewY: 0,
+      text: arr.text.value.replace(/(?:\\[rn]|[\r\n]+)+|\u0003/g, "\n"),
+      fontSize: arr.text.font.sizes[0],
+      fontWeight: "",
+      fontFamily: arr.text.font.name.trim(),
+      fontStyle: "",
+      lineHeight: 1,
+      underline: "",
+      overline: false,
+      linethrough: false,
+      textAlign: arr.text.font.alignment[0],
+      textBackgroundColor: "",
+      charSpacing: 0,
+      styles: {},
+    };
+    return dt;
+  }
+  var dt = {
+    type: "image",
+    version: "4.1.0",
+    originX: "left",
+    originY: "top",
+    left: arr.left,
+    top: arr.top,
+    width: arr.width,
+    height: arr.height,
+    fill: "rgb(0,0,0)",
+    stroke: null,
+    strokeWidth: 0,
+    strokeDashArray: null,
+    strokeLineCap: "butt",
+    strokeDashOffset: 0,
+    strokeLineJoin: "miter",
+    strokeMiterLimit: 4,
+    scaleX: 1,
+    scaleY: 1,
+    angle: 0,
+    flipX: false,
+    flipY: false,
+    opacity: 100,
+    shadow: null,
+    visible: true,
+    backgroundColor: "",
+    fillRule: "nonzero",
+    paintFirst: "fill",
+    globalCompositeOperation: "source-over",
+    skewX: 0,
+    skewY: 0,
+    cropX: 0,
+    cropY: 0,
+    crossOrigin: null,
+    filters: [],
+    src: layer.get("image").toBase64(scale),
+  };
+  return dt;
+}
+
+function getFabricJson(width, height) {
+  return {
+    version: "4.1.0",
+    objects: [],
+    backgroundImage: {
+      type: "image",
+      version: "4.1.0",
+      originX: "left",
+      originY: "top",
+      left: 0,
+      top: 0,
+      width: width,
+      height: height,
+      fill: "rgb(255,255,255)",
+      stroke: null,
+      strokeWidth: 0,
+      strokeDashArray: null,
+      strokeLineCap: "butt",
+      strokeDashOffset: 0,
+      strokeLineJoin: "miter",
+      strokeMiterLimit: 4,
+      scaleX: 1,
+      scaleY: 1,
+      angle: 0,
+      flipX: false,
+      flipY: false,
+      opacity: 1,
+      shadow: null,
+      visible: true,
+      backgroundColor: "",
+      fillRule: "nonzero",
+      paintFirst: "fill",
+      globalCompositeOperation: "source-over",
+      skewX: 0,
+      skewY: 0,
+      cropX: 0,
+      cropY: 0,
+      crossOrigin: "anonymous",
+      filters: [],
+    },
+  };
+}
+
+async function convertPSD(file) {
+  var psd = await PSD.fromDroppedFile(file);
+  // console.log("PSD");
+  // console.log(psd);
+  var tree = psd.tree();
+  console.log(JSON.stringify(tree.export()));
+  var abcd = tree.descendants();
+  var w = tree.get("width");
+  var h = tree.get("height");
+  var sc = 1;
+  // var sc = Math.min(1, 1000 / w);
+  var dt = getFabricJson(w * sc, h * sc);
+  for (let i = 0; i < abcd.length; i++) {
+    if (
+      abcd[i].isGroup() ||
+      !abcd[i].export().visible ||
+      abcd[i].get("width") < 1
+    )
+      continue;
+    if (abcd[i].get("width") >= w && abcd[i].get("height") >= h) {
+      dt.backgroundImage.src = abcd[i].get("image").toBase64(sc);
+      break;
+    } else {
+      dt.objects.push(toFabricJson(abcd[i], sc));
+    }
+  }
+  dt.objects = dt.objects.reverse();
+  // console.log("DATA");
+  // console.log(dt);
+  return dt;
+}
